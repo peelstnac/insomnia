835e9f7446a2bf952081c2ea963ab8f019b82e9b
peelstnac <chfrn2002@gmail.com>
2020-06-16 21:30:16 -0400

    Adding enemies

--- server.js
+++ server.js
@@ -1,25 +1,53 @@
+const app = require('express')();
 const express = require('express');
-const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server, {});
+const io = require('socket.io')(server);
 
-app.use(express.static(__dirname + '/public'));
-app.on('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-})
-app.listen(3000);
+const WIDTH = 750;
+const HEIGHT = 750;
+const PI = 3.141582;
 
-class Player {
-    constructor(x, y, vel, hp) {
-        x = Math.floor(Math.random() * 500);
-        y = Math.floor(Math.random() * 500);
-        vel = 10;
-        hp = 100;
+var enemyCount = 0;
+var enemyList = [];
+
+class Enemy {
+    constructor(name) {
+        this.name = name;
+    }
+    x = 750/2;
+    y = 750/2;
+    vel = Math.floor(Math.random() * 10) + 10;
+    ang = Math.random() * PI;
+    dx = Math.cos(ang) * vel;
+    dy = Math.sin(ang) * vel;
+}
+
+function updateEnemies(enemyList) {
+    for(var enemy in enemyList) {
+
     }
 }
 
-players = {};
+app.use(express.static(__dirname + '/public'));
+server.listen(3000);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 io.on('connection', (socket) => {
-
+  
 });
+
+setInterval(() => {
+    var packet = {};
+    updateEnemies(enemyList);
+    //generate enemies if there are less than 10
+    while(enemyCount < 10) {
+        enemyCount++;
+        enemyList.push(new Enemy('test'));
+    }
+    packet.enemyList = enemyList;
+
+    io.emit('state', packet);
+}, 1000/30);
