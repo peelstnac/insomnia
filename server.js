de09feed807a882ef48dadb0dccfdbcd38f4601e
peelstnac <chfrn2002@gmail.com>
2020-06-19 14:10:44 -0400

    Fixed player collisions

--- server.js
+++ server.js
@@ -65,13 +65,6 @@ class Player {
     weapon = 0; //type of weapon equipped
 
     updatePosition() {
-        //collision detection
-        if(this.x <= 0 || this.x + this.dim/2 >= WIDTH) {
-            this.dx = 0;
-        }
-        if(this.y <= 0 || this.y + this.dim/2 >= HEIGHT) {
-            this.dy = 0;
-        }
         this.x += this.dx;
         this.y += this.dy;
     }
@@ -142,10 +135,10 @@ io.on('connection', (socket) => {
         playerList[id].dx = 0;
         playerList[id].dy = 0;
         if(keyArray[0]) {
-            if(playerList[id].y >= 32/2) playerList[id].dy = -playerList[id].vel / Math.sqrt(2);
+            if(playerList[id].y >= HEIGHT + 32/2) playerList[id].dy = -playerList[id].vel / Math.sqrt(2);
         }
         if(keyArray[1]) {
-            if(playerList[id].x >= 32/2) playerList[id].dx = -playerList[id].vel / Math.sqrt(2);
+            if(playerList[id].x >= WIDTH + 32/2) playerList[id].dx = -playerList[id].vel / Math.sqrt(2);
         }
         if(keyArray[2]) {
             if(playerList[id].y <= HEIGHT - 32/2) playerList[id].dy = playerList[id].vel / Math.sqrt(2);
