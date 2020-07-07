8e46b84f26301fb17cc1adbeca5515f7b769fddc
peelstnac <chfrn2002@gmail.com>
2020-07-06 21:15:20 -0400

    Added new features, fixed bugs

---
+++ public/script.js
@@ -0,0 +1,160 @@
+"use strict";
+
+var c = document.getElementById("ctx");
+var ctx = c.getContext("2d");
+const socket = io();
+const WIDTH = 1280;
+const HEIGHT = 720;
+
+var cEnemyList = [];
+var cProjectileList = [];
+var cPlayerList = [];
+var cStatList = [];
+var projectiles = [];
+
+
+var id;
+socket.on('id', (data) => {
+    id = data.id;
+});
+
+//handle cursor
+var mx = 0;
+var my = 0;
+//handle keys
+var keyArray = [0, 0, 0, 0, 0]; //w, a, s, d, space
+socket.on('controllablePositions', (data) => {
+    cPlayerList = data.cPlayerList;
+});
+socket.on('state', (data) => {
+    //console.log(data);
+    //parse data
+    cEnemyList = data.cEnemyList;
+    cStatList = data.cStatList;
+    /*
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    ctx.fillStyle = 'red';
+    for(var enemy in enemyList) {
+        ctx.fillText('enemy', enemyList[enemy].x-25, enemyList[enemy].y-30);
+        ctx.fillRect(enemyList[enemy].x-25, enemyList[enemy].y-25, 50, 50);
+        //console.log(enemy.x);
+    }
+    */
+    //parse data
+    /*
+    ctx.fillStyle = 'green';
+    for(var player in playerList) {
+        ctx.fillRect(playerList[player].x-16, playerList[player].y-16, 32, 32);
+    }
+    */
+    //parse data
+
+    /*
+    var projectileList = data.projectileList;
+    //console.log(projectileList);
+    for(var proj in projectileList) {
+        ctx.fillRect(projectileList[proj].x - projectileList[proj].dim/2, 
+        projectileList[proj].y - projectileList[proj].dim/2, projectileList[proj].dim, projectileList[proj].dim);
+    }
+    */
+
+    cProjectileList = data.cProjectileList;
+
+    //display ammo
+    /*
+    $('#ammo').empty();
+    $('#ammo').append(playerList[id].ammo);
+    */
+});
+//handle mouse
+c.addEventListener('mousemove', (event) => {
+    var rect = c.getBoundingClientRect();
+    mx = event.clientX - rect.left;
+    my = event.clientY - rect.top;
+});
+//handle keys
+document.onkeydown = (event) => {
+    if(event.keyCode === 87) {
+        keyArray[0] = 1;
+    }
+    if(event.keyCode === 65) {
+        keyArray[1] = 1;
+    }
+    if(event.keyCode === 83) {
+        keyArray[2] = 1;
+    }
+    if(event.keyCode === 68) {
+        keyArray[3] = 1;
+    }
+    if(event.keyCode == 32) {
+        keyArray[4] = 1;
+    }
+}
+document.onkeyup = (event) => {
+    if(event.keyCode === 87) {
+        keyArray[0] = 0;
+    }
+    if(event.keyCode === 65) {
+        keyArray[1] = 0;
+    }
+    if(event.keyCode === 83) {
+        keyArray[2] = 0;
+    }
+    if(event.keyCode === 68) {
+        keyArray[3] = 0;
+    }
+    if(event.keyCode == 32) {
+        keyArray[4] = 0;
+    }
+}
+//interpolate data
+setInterval(() => {
+    //send key data
+    var packet = {};
+    packet.keyArray = keyArray;
+    //TODO: merge the two emits to improve efficiency
+    socket.emit('keyPress', packet);
+    //send mouse data
+    packet = {
+        x: mx,
+        y: my
+    };
+    socket.emit('cursorMove', packet);
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    //handle enemies
+    ctx.fillStyle = 'red';
+    for(var enemy in cEnemyList) {
+        //update position
+        cEnemyList[enemy].x += cEnemyList[enemy].dx/2;
+        cEnemyList[enemy].y += cEnemyList[enemy].dy/2;
+        //draw
+        ctx.fillText('enemy', cEnemyList[enemy].x-25, cEnemyList[enemy].y-30);
+        ctx.fillRect(cEnemyList[enemy].x-25, cEnemyList[enemy].y-25, 50, 50);
+        //console.log(enemy.x);
+    }
+    //handle players
+    ctx.fillStyle = 'green';
+    for(var player in cPlayerList) {
+        //draw
+        ctx.fillRect(cPlayerList[player].x-16, cPlayerList[player].y-16, 32, 32);
+    }
+    //handle projectiles
+    ctx.fillStyle = 'blue';
+    for(var proj in cProjectileList) {
+        projectiles.push(cProjectileList[proj]);
+    }
+    cProjectileList = [];
+    for(var proj = projectiles.length-1; proj >= 0; proj--) {
+        //update position
+        if(projectiles[proj].timer <= 0) {
+            projectiles.splice(proj, 1);
+            continue;
+        }
+        projectiles[proj].x += projectiles[proj].dx/2;
+        projectiles[proj].y += projectiles[proj].dy/2;
+        projectiles[proj].timer -= 0.5;
+        //draw
+        ctx.fillRect(projectiles[proj].x - 3, 
+        projectiles[proj].y - 3, 6, 6);
+    }
+}, 1000/30); //30 FPS simulated
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const app = require('express')();
 const express = require('express');
 const server = require('http').Server(app);
@@ -6,271 +8,297 @@ const { v4: uuidv4 } = require('uuid');
 
 const WIDTH = 1280;
 const HEIGHT = 720;
-const PI = 3.141582;
+const PI = Math.PI;
 
-var enemyCount = 0;
-var enemyList = [];
+const Level = require('./level.js');
 
-class Enemy {
-    constructor(name) {
-        this.name = name;
-    }
-    //enemies spawn in the middle of map
-    x = WIDTH/2;
-    y = HEIGHT/2;
-    vel = 15;
-    //random trajectory
-    ang = Math.random() * PI * 2;
-    dx = Math.cos(this.ang) * this.vel;
-    dy = Math.sin(this.ang) * this.vel;
-    //fixed size for now
-    dim = 50;
-    hp = 20;
+(() => {
+    //handle day and night cycle
+    var time = 1; //day 1, night 0
+    setInterval(() => {
+        time = ~time&1;
+    }, 90000);
 
-    updatePosition() {
-        //collision detection
-        if(this.x <= this.dim/2 || this.x + this.dim/2 >= WIDTH) {
-            this.dx = -this.dx;
+    var enemyCount = 0;
+    var enemyList = [];
+
+    class Enemy {
+        constructor(name, enemyStrengthIndex) {
+            this.name = name;
+            this.enemyStrengthIndex = enemyStrengthIndex;
+            this.hp = 19 + this.enemyStrengthIndex;
         }
-        if(this.y <= this.dim/2 || this.y + this.dim/2 >= HEIGHT) {
-            this.dy = -this.dy;
-        }
-        this.x += this.dx;
-        this.y += this.dy;
-    }
-}
+        //enemies spawn in the middle of map
+        x = WIDTH/2;
+        y = HEIGHT/2;
+        vel = 15;
+        //random trajectory
+        ang = Math.random() * PI * 2;
+        dx = Math.cos(this.ang) * this.vel;
+        dy = Math.sin(this.ang) * this.vel;
+        //fixed size for now
+        dim = 50;
 
-//list of clients
-var playerList = {};
-
-//ammo cost of different weapons
-const ammo_cost = [1];
-
-class Player {
-    //each player has unique ID
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-    }
-    //random spawn point
-    x = Math.floor(Math.random() * WIDTH);
-    y = Math.floor(Math.random() * HEIGHT);
-    vel = 15;
-    ang = 0; //mouse angle
-    dx = 0;
-    dy = 0;
-    dim = 32;
-    hp = 150;
-    ammo = 300;
-    weapon = 0; //type of weapon equipped
-
-    updatePosition() {
-        this.x += this.dx;
-        this.y += this.dy;
-    }
-}
-
-class Projectile {
-    constructor(x, y, vel, ang, dim, timer, parent, dmg) {
-        this.x = x;
-        this.y = y;
-        this.vel = vel;
-        this.ang = ang;
-        this.dim = dim;
-        this.timer = timer;
-        this.parent = parent;
-        this.dmg = dmg;
-        this.dx = Math.cos(this.ang) * this.vel;
-        this.dy = Math.sin(this.ang) * this.vel;
-    }
-
-    updatePosition() {
-        this.timer--;
-        /*
-        if(this.x <= 0 || this.x + this.dim/2 >= WIDTH) {
-            this.dx = -this.dx;
-        }
-        if(this.y <= 0 || this.y + this.dim/2 >= HEIGHT) {
-            this.dy = -this.dy;
-        }
-        */
-        this.x += this.dx;
-        this.y += this.dy;
-    }
-}
-
-var projectileList = [];
-var cProjectileList = []; //lightweight client version
-
-app.use(express.static(__dirname + '/public'));
-server.listen(process.env.PORT || 3000);
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-//list of sockets to send player specific information
-var socketList = {};
-
-io.on('connection', (socket) => {
-    console.log('player connect.');
-    var id = uuidv4(); //create unique ID for player
-    socketList[id] = socket;
-    playerList[id] = new Player(id, 'hope');
-
-    socket.on('disconnect', () => {
-        console.log('player disconnect.');
-        delete socketList[id];
-        delete playerList[id];
-    });
-
-    socket.on('cursorMove', (data) => {
-        //calculate angle of cursor relative to player position
-        var ang = Math.atan2(data.y - playerList[id].y, data.x - playerList[id].x);
-        playerList[id].ang = ang;
-    });
-
-    socket.on('keyPress', (data) => {
-        var keyArray = data.keyArray;
-        playerList[id].dx = 0;
-        playerList[id].dy = 0;
-        if(keyArray[0]) {
-            if(playerList[id].y >= 32/2) playerList[id].dy = -playerList[id].vel / Math.sqrt(2);
-        }
-        if(keyArray[1]) {
-            if(playerList[id].x >= 32/2) playerList[id].dx = -playerList[id].vel / Math.sqrt(2);
-        }
-        if(keyArray[2]) {
-            if(playerList[id].y <= HEIGHT - 32/2) playerList[id].dy = playerList[id].vel / Math.sqrt(2);
-        }
-        if(keyArray[3]) {
-            if(playerList[id].x <= WIDTH - 32/2) playerList[id].dx = playerList[id].vel / Math.sqrt(2);
-        }
-        //shoot key
-        if(keyArray[4]) {
-            if(playerList[id].ammo >= ammo_cost[playerList[id].weapon]) {
-                var proj = new Projectile(playerList[id].x, playerList[id].y, 45, playerList[id].ang, 6, 15, id, 5);
-                //trying to reduce socket load
-                projectileList.push(proj);
-                //less information for client
-                cProjectileList.push({
-                    x: proj.x,
-                    y: proj.y,
-                    dx: proj.dx,
-                    dy: proj.dy,
-                    timer: proj.timer
-                });
-                //decrement ammo by ammo cost of weapon
-                playerList[id].ammo -= ammo_cost[playerList[id].weapon];
+        updatePosition() {
+            //collision detection
+            if(this.x <= this.dim/2 || this.x + this.dim/2 >= WIDTH) {
+                this.dx = -this.dx;
             }
+            if(this.y <= this.dim/2 || this.y + this.dim/2 >= HEIGHT) {
+                this.dy = -this.dy;
+            }
+            this.x += this.dx;
+            this.y += this.dy;
         }
-    });
-});
+    }
 
-//controllable positions are updated at 30 FPS
-setInterval(() => {
-    var cPlayerList = []; //client version
-    for(var player in playerList) {
-        playerList[player].updatePosition(); 
-        cPlayerList.push({
-            x: playerList[player].x,
-            y: playerList[player].y
+    //list of clients
+    var playerList = {};
+
+    //ammo cost of different weapons
+    const ammo_cost = [1];
+
+    class Player {
+        //each player has unique ID
+        constructor(id, name) {
+            this.id = id;
+            this.name = name;
+        }
+        //random spawn point
+        x = Math.floor(Math.random() * WIDTH);
+        y = Math.floor(Math.random() * HEIGHT);
+        vel = 15;
+        ang = 0; //mouse angle
+        dx = 0;
+        dy = 0;
+        dim = 32;
+        hp = 150;
+        kills = 0;
+        deaths = 0;
+        ammo = 300;
+        weapon = 0; //type of weapon equipped
+
+        updatePosition() {
+            this.x += this.dx;
+            this.y += this.dy;
+        }
+    }
+
+    class Projectile {
+        constructor(x, y, vel, ang, dim, timer, parent, dmg) {
+            this.x = x;
+            this.y = y;
+            this.vel = vel;
+            this.ang = ang;
+            this.dim = dim;
+            this.timer = timer;
+            this.parent = parent;
+            this.dmg = dmg;
+            this.dx = Math.cos(this.ang) * this.vel;
+            this.dy = Math.sin(this.ang) * this.vel;
+        }
+
+        updatePosition() {
+            this.timer--;
+            /*
+            if(this.x <= 0 || this.x + this.dim/2 >= WIDTH) {
+                this.dx = -this.dx;
+            }
+            if(this.y <= 0 || this.y + this.dim/2 >= HEIGHT) {
+                this.dy = -this.dy;
+            }
+            */
+            this.x += this.dx;
+            this.y += this.dy;
+        }
+    }
+
+    var projectileList = [];
+    var cProjectileList = []; //lightweight client version
+
+    app.use(express.static(__dirname + '/public'));
+    server.listen(process.env.PORT || 3000);
+
+    app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+    });
+
+    //list of sockets to send player specific information
+    var socketList = {};
+
+    io.on('connection', (socket) => {
+        console.log('player connect.');
+        var id = uuidv4(); //create unique ID for player
+        socketList[id] = socket;
+        playerList[id] = new Player(id, 'hope');
+
+        socket.on('disconnect', () => {
+            console.log('player disconnect.');
+            delete socketList[id];
+            delete playerList[id];
         });
-    }
-    var packet = {
-        cPlayerList: cPlayerList
-    };
-    io.emit('controllablePositions', packet);
-}, 1000/30);
 
-//everything else is updated at 15 FPS and interpolated to 30 FPS on client
-setInterval(() => {
-    //collision damage detection
-    for(var enemy in enemyList) {
-        for(var player in playerList) {
-            var ex = enemyList[enemy].x;
-            var ey = enemyList[enemy].y;
-            var px = playerList[player].x;
-            var py = playerList[player].y;
+        socket.on('cursorMove', (data) => {
+            //calculate angle of cursor relative to player position
+            var ang = Math.atan2(data.y - playerList[id].y, data.x - playerList[id].x);
+            playerList[id].ang = ang;
+        });
 
-            if(Math.abs(ex - px) < 41 && Math.abs(ey - py) < 41) {
-                playerList[player].hp -= enemyList[enemy].hp;
+        socket.on('keyPress', (data) => {
+            var keyArray = data.keyArray;
+            playerList[id].dx = 0;
+            playerList[id].dy = 0;
+            if(keyArray[0]) {
+                if(playerList[id].y >= 32/2) playerList[id].dy = -playerList[id].vel / Math.sqrt(2);
             }
-        }
-    }
-    for(var proj in projectileList) {
-        var bx = projectileList[proj].x;
-        var by = projectileList[proj].y;
-        for(var player in playerList) {
-            if(player == projectileList[proj].parent) continue;
-            var px = playerList[player].x;
-            var py = playerList[player].y;
-            if(Math.abs(bx - px) < 16+projectileList[proj].dim/2 && Math.abs(by - py) < 16+projectileList[proj].dim/2) {
-                playerList[player].hp -= projectileList[proj].dmg;
-                if(playerList[player].hp <= 0) {
-                    //transfer ammo to killer
-                    playerList[projectileList[proj].parent].ammo[0] += playerList[player].ammo[0];
+            if(keyArray[1]) {
+                if(playerList[id].x >= 32/2) playerList[id].dx = -playerList[id].vel / Math.sqrt(2);
+            }
+            if(keyArray[2]) {
+                if(playerList[id].y <= HEIGHT - 32/2) playerList[id].dy = playerList[id].vel / Math.sqrt(2);
+            }
+            if(keyArray[3]) {
+                if(playerList[id].x <= WIDTH - 32/2) playerList[id].dx = playerList[id].vel / Math.sqrt(2);
+            }
+            //shoot key
+            if(keyArray[4]) {
+                if(playerList[id].ammo >= ammo_cost[playerList[id].weapon]) {
+                    var proj = new Projectile(playerList[id].x, playerList[id].y, 45, playerList[id].ang, 6, 15, id, 5);
+                    //trying to reduce socket load
+                    projectileList.push(proj);
+                    //less information for client
+                    cProjectileList.push({
+                        x: proj.x,
+                        y: proj.y,
+                        dx: proj.dx,
+                        dy: proj.dy,
+                        timer: proj.timer
+                    });
+                    //decrement ammo by ammo cost of weapon
+                    playerList[id].ammo -= ammo_cost[playerList[id].weapon];
                 }
             }
+        });
+    });
+
+    //controllable positions are updated at 30 FPS
+    setInterval(() => {
+        var cPlayerList = []; //client version
+        for(var player in playerList) {
+            playerList[player].updatePosition(); 
+            cPlayerList.push({
+                x: playerList[player].x,
+                y: playerList[player].y
+            });
         }
+        var packet = {
+            cPlayerList: cPlayerList
+        };
+        io.emit('controllablePositions', packet);
+    }, 1000/30);
+
+    //everything else is updated at 15 FPS and interpolated to 30 FPS on client
+    setInterval(() => {
+        //collision damage detection
         for(var enemy in enemyList) {
-            var ex = enemyList[enemy].x;
-            var ey = enemyList[enemy].y;
-            if(Math.abs(bx - ex) < 25+projectileList[proj].dim/2 && Math.abs(by - ey) < 25+projectileList[proj].dim/2) {
-                enemyList[enemy].hp -= projectileList[proj].dmg;
-                if(enemyList[enemy].hp <= 0) {
-                    //give killer 30 ammo
-                    playerList[projectileList[proj].parent].ammo[0] += 15;
+            for(var player in playerList) {
+                var ex = enemyList[enemy].x;
+                var ey = enemyList[enemy].y;
+                var px = playerList[player].x;
+                var py = playerList[player].y;
+
+                if(Math.abs(ex - px) < 41 && Math.abs(ey - py) < 41) {
+                    playerList[player].hp -= enemyList[enemy].hp;
                 }
             }
         }
-    }
-    //check if player is alive
-    for(var player in playerList) {
-        if(playerList[player].hp <= 0) {
-            playerList[player] = new Player(player, playerList[player].name);
+        for(var proj in projectileList) {
+            var bx = projectileList[proj].x;
+            var by = projectileList[proj].y;
+            for(var player in playerList) {
+                if(player == projectileList[proj].parent) continue;
+                var px = playerList[player].x;
+                var py = playerList[player].y;
+                if(Math.abs(bx - px) < 16+projectileList[proj].dim/2 && Math.abs(by - py) < 16+projectileList[proj].dim/2) {
+                    playerList[player].hp -= projectileList[proj].dmg;
+                    if(playerList[player].hp <= 0) {
+                        //transfer ammo to killer
+                        playerList[projectileList[proj].parent].ammo += playerList[player].ammo;
+                    }
+                }
+            }
+            for(var enemy in enemyList) {
+                var ex = enemyList[enemy].x;
+                var ey = enemyList[enemy].y;
+                if(Math.abs(bx - ex) < 25+projectileList[proj].dim/2 && Math.abs(by - ey) < 25+projectileList[proj].dim/2) {
+                    enemyList[enemy].hp -= projectileList[proj].dmg;
+                    if(enemyList[enemy].hp <= 0) {
+                        //give killer 30 ammo
+                        playerList[projectileList[proj].parent].ammo += 15;
+                    }
+                }
+            }
         }
-    }
-    //check if enemy is alive
-    for(var enemy in enemyList) {
-        if(enemyList[enemy].hp <= 0) {
-            enemyList[enemy] = new Enemy('enemy');
+        //check if player is alive
+        for(var player in playerList) {
+            if(playerList[player].hp <= 0) {
+                playerList[player] = new Player(player, playerList[player].name);
+            }
         }
-    }
-    //check if projectile is alive
-    for(var i = projectileList.length-1; i >= 0; i--) {
-        if(projectileList[i].timer <= 0) {
-            projectileList.splice(i, 1); //if not, remove
+        //check if enemy is alive
+        for(var enemy in enemyList) {
+            if(enemyList[enemy].hp <= 0) {
+                enemyList[enemy] = new Enemy('enemy');
+            }
         }
-    }
-    var packet = {};
-    //update enemy positions
-    for(enemy in enemyList) {
-        enemyList[enemy].updatePosition();
-    }
-    //generate enemies if there are less than 10
-    while(enemyCount < 10) {
-        enemyCount++;
-        enemyList.push(new Enemy('test'));
-    }
-    //update projectile positions
-    for(proj in projectileList) {
-        projectileList[proj].updatePosition();
-    }
+        //check if projectile is alive
+        for(var i = projectileList.length-1; i >= 0; i--) {
+            if(projectileList[i].timer <= 0) {
+                projectileList.splice(i, 1); //if not, remove
+            }
+        }
+        var packet = {};
+        //update enemy positions
+        for(enemy in enemyList) {
+            enemyList[enemy].updatePosition();
+        }
+        //generate enemies if there are less than 10
+        while(enemyCount < 10) {
+            enemyCount++;
+            enemyList.push(new Enemy('test'));
+        }
+        //update projectile positions
+        for(proj in projectileList) {
+            projectileList[proj].updatePosition();
+        }
 
-    var cEnemyList = [];
-    
-    for(var enemy in enemyList) {
-        cEnemyList.push({
-            x: enemyList[enemy].x,
-            y: enemyList[enemy].y,
-            dx: enemyList[enemy].dx,
-            dy: enemyList[enemy].dy
-        });
-    }
+        var cEnemyList = [];
+        
+        for(var enemy in enemyList) {
+            cEnemyList.push({
+                x: enemyList[enemy].x,
+                y: enemyList[enemy].y,
+                dx: enemyList[enemy].dx,
+                dy: enemyList[enemy].dy
+            });
+        }
 
-    packet.cEnemyList = cEnemyList;
-    packet.cProjectileList = cProjectileList;
-    io.emit('state', packet);
-    cProjectileList = []; //clear the client list to reduce load 
-}, 1000/15);
+        var cStatList = [];
+        for(let player in playerList) {
+            cStatList.push({
+                hp: playerList[player].hp,
+                ammo: playerList[player].ammo,
+                weapon: playerList[player].weapon,
+                kills: playerList[player].kills,
+                deaths: playerList[player].deaths
+            });
+        }
+
+        packet.cEnemyList = cEnemyList;
+        packet.cProjectileList = cProjectileList;
+        packet.cStatList = cStatList;
+        
+        io.emit('state', packet);
+        cProjectileList = []; //clear the client list to reduce load 
+    }, 1000/15);
+})();
